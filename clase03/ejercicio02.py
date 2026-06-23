2b93744b2040fd701dd0a187ba76865ee461db18
lucasandresponce <lucasandresponce@gmail.com>
2026-06-23 12:12:14 -0300

    agregue ejercicios

---
+++ clase03/ejercicio02.py
@@ -0,0 +1,20 @@
+nombres = ["maria","pablo","ada"]
+
+for nombre in nombres:
+    print(nombre)
+
+
+
+numeros = [5,8,10,20]
+
+for num in numeros:
+    print(num * 2)
+
+frutas = ["manzana","pera","limon"]
+
+for fruta in frutas:
+
+    if fruta == "limon":
+        print(f"{fruta}es")
+    else:
+        print(f"{fruta}s")
